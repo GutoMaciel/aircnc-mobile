fce1803f9fb3f778266ff3c4f57e2b8d235cd713
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-11-30 12:50:08 -0300

    List page, book confirmation

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -1,6 +1,114 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+import api from '../services/api';
+import {
+  SafeAreaView,
+  AsyncStorage,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 
-export default function Book() {
-  return <View />;
+export default function Book({navigation}) {
+  const [date, setDate] = useState('');
+  const id = navigation.getParam('id');
+
+  async function handleSubmit() {
+    const user_id = await AsyncStorage.getItem('user');
+
+    await api.post(
+      `/spots/${id}/bookings`,
+      {
+        date,
+      },
+      {
+        headers: {user_id},
+      },
+    );
+
+    Alert.alert('Request was sent to the company');
+
+    navigation.navigate('List');
+  }
+
+  function handleCancel() {
+    navigation.navigate('List');
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.label}>Date to request</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Witch date do you want to request?"
+        placeholderTextColor="#999"
+        autoCapitalize="words"
+        autoCorrect={false}
+        value={date}
+        onChangeText={setDate}
+      />
+
+      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+        <Text style={styles.buttonText}>Request spot</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={handleCancel} style={styles.cancelButton}>
+        <Text style={styles.buttonText}>Cancel</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 30,
+  },
+
+  label: {
+    fontWeight: 'bold',
+    color: '#444',
+    marginBottom: 8,
+    marginTop: 30,
+  },
+
+  form: {
+    alignSelf: 'stretch',
+    paddingHorizontal: 30,
+    marginTop: 30,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: '#444',
+    height: 44,
+    marginBottom: 20,
+    borderRadius: 2,
+  },
+
+  button: {
+    height: 42,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2,
+  },
+
+  cancelButton: {
+    height: 42,
+    marginTop: 10,
+    backgroundColor: '#ccc',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2,
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
