caade67aca7ab14ae50366c966b7014bd99c00a4
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-12-02 13:59:56 -0300

    feature: socket io mobile integration

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react';
+import socketio from 'socket.io-client';
 import {
   AsyncStorage,
   Image,
   SafeAreaView,
   StyleSheet,
   ScrollView,
+  Alert,
 } from 'react-native';
 
 import SpotList from '../components/SpotList';
@@ -13,6 +15,23 @@ import logo from '../assets/logo.png';
 
 export default function List() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user_id => {
+      const socket = socketio('http://localhost:3333', {
+        query: {user_id},
+      });
+
+      socket.on('booking_response', booking => {
+        Alert.alert(
+          `Your request at ${booking.spot.company} for ${booking.date} was ${
+            booking.approved ? 'Approved' : 'Rejected'
+          }`,
+        );
+      });
+    });
+  }, []);
+
   useEffect(() => {
     AsyncStorage.getItem('techs').then(storageTechs => {
       const techsArray = storageTechs.split(',').map(tech => tech.trim());
